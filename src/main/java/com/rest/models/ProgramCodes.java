afa63197dc006ff42bbf698fc079ee4ce31e952a
Git1826 <chiragverma1826@gmail.com>
2024-04-17 10:32:54 +0530

    no message

---
+++ src/main/java/com/rest/models/ProgramCodes.java
@@ -0,0 +1,31 @@
+package com.rest.models;
+
+public class ProgramCodes {
+
+private String title;
+private String description;
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+@Override
+public String toString() {
+	return "ProgramCodes [title=" + title + ", description=" + description + "]";
+}
+public ProgramCodes(String title, String description) {
+	super();
+	this.title = title;
+	this.description = description;
+}
+public ProgramCodes() {
+	super();
+}
+}
