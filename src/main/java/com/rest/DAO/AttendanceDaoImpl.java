1a01020ea478b06dccc4b681f768f79017515ad8
Git1826 <chiragverma1826@gmail.com>
2024-04-24 15:52:50 +0530

    no message

--- src/main/java/com/rest/DAO/AttendanceDaoImpl.java
+++ src/main/java/com/rest/DAO/AttendanceDaoImpl.java
@@ -406,7 +406,12 @@ public class AttendanceDaoImpl implements AttendanceDao {
 for(int i =0;i<employeesDataAbsentAttendance.size();i++) {
 	pdfGenerateReport obj = new pdfGenerateReport(employeesDataAbsentAttendance.get(i).getPbId(), employeesDataAbsentAttendance.get(i).getName(), employeesDataAbsentAttendance.get(i).getDesignation(), employeesDataAbsentAttendance.get(i).getDivision());
 	obj.setDate(employeesDataAbsentAttendance.get(i).getDate());
-	pdfReport.add(obj);
+	for(int j = 0;j< pdfReport.size();j++) {
+		if(pdfReport.get(i).equals(obj)) {
+			pdfReport.remove(pdfReport.get(i));
+		}
+	}
+
 }
 		
 
