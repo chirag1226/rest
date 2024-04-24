fc9321f417fbfc0958f6d5e7e0ce62b19b56818f
Git1826 <chiragverma1826@gmail.com>
2024-04-24 13:54:49 +0530

    no message

---
+++ src/main/java/com/rest/models/pdfGenerateReport.java
@@ -0,0 +1,68 @@
+package com.rest.models;
+
+public class pdfGenerateReport {
+	private String pbId;
+	private String name;
+	private String designation;
+	private String division;
+	private String date;
+	private String inPunch;
+	private String outPunch;
+	public String getPbId() {
+		return pbId;
+	}
+	public void setPbId(String pbId) {
+		this.pbId = pbId;
+	}
+
+	public pdfGenerateReport(String pbId, String name, String designation, String division) {
+		super();
+		this.pbId = pbId;
+		this.name = name;
+		this.designation = designation;
+		this.division = division;
+		
+
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public pdfGenerateReport() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+	public void setDivision(String division) {
+		this.division = division;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getInPunch() {
+		return inPunch;
+	}
+	public void setInPunch(String inPunch) {
+		this.inPunch = inPunch;
+	}
+	public String getOutPunch() {
+		return outPunch;
+	}
+	public void setOutPunch(String outPunch) {
+		this.outPunch = outPunch;
+	}
+}
+
